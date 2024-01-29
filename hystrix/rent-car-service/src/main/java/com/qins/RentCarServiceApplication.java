fcda2bc44e9b4cecd54ef76a9339cb3794ab996b
qins <2941634984@qq.com>
2024-01-29 15:37:26 +0800

    1

--- hystrix/rent-car-service/src/main/java/com/qins/RentCarServiceApplication.java
+++ hystrix/rent-car-service/src/main/java/com/qins/RentCarServiceApplication.java
@@ -1,9 +1,11 @@
 package com.qins;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class RentCarServiceApplication {
 
     public static void main(String[] args) {
