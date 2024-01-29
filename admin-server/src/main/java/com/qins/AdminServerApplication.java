53aa985e91161362169069ffda6e7d29b68118ba
qins <2941634984@qq.com>
2024-01-29 15:19:17 +0800

    1

--- admin-server/src/main/java/com/qins/AdminServerApplication.java
+++ admin-server/src/main/java/com/qins/AdminServerApplication.java
@@ -1,9 +1,13 @@
 package com.qins;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
