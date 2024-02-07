f1bd64bb5ae55f00fbd563956688b92a16ed0bbb
qins <2941634984@qq.com>
2024-02-07 12:52:18 +0800

    1

---
+++ redis1/src/main/java/com/qins/Redis1Application.java
@@ -0,0 +1,13 @@
+package com.qins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redis1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Redis1Application.class, args);
+    }
+
+}
