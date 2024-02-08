87bc54bcd60d8a8b9ebf64c1b953b8737651ebee
qins <2941634984@qq.com>
2024-02-08 10:01:02 +0800

    1

---
+++ redis1/src/main/java/com/qins/TestTx.java
@@ -0,0 +1,38 @@
+package com.qins;
+
+import com.alibaba.fastjson2.JSONObject;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
+public class TestTx {
+
+    public static void main(String[] args) {
+        Jedis jedis=new Jedis("127.0.0.1",6379);
+
+
+        Transaction multi = jedis.multi();
+//        jedis.watch("name");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("hello","world");
+        jsonObject.put("qins","s");
+        String s = jsonObject.toJSONString();
+
+        jedis.watch(s);
+        try {
+            multi.set("user1",s);
+            multi.set("user2",s);
+//            int i=1/0;
+            multi.exec();
+
+        }catch (Exception e){
+            multi.discard();
+
+        }finally {
+            System.out.println(jedis.get("user1"));
+            System.out.println(jedis.get("user2"));
+            jedis.close();
+        }
+
+    }
+}
