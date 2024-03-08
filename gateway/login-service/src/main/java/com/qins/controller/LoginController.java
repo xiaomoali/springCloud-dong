c6abdfe0f83c03f2f7aef77845867dedc4b217f4
qins <2941634984@qq.com>
2024-03-08 10:34:00 +0800

    1

---
+++ gateway/login-service/src/main/java/com/qins/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.qins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.UUID;
+
+public class LoginController {
+
+
+    @GetMapping("/doLogin")
+    public String doLogin(String name,String pwd){
+
+        System.out.println(name);
+        System.out.println(pwd);
+
+        String token = UUID.randomUUID().toString();
+        return token;
+    }
+}
